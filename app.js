0fa17ffbcaa19c949c055ad0c19cd35949615b2d
samarth joshi <samarthj5696@gmail.com>
2023-07-17 01:15:48 +0530

    css delay loading fixed

--- app.js
+++ app.js
@@ -14,17 +14,6 @@ hiddenElements.forEach((el) => {
 });
 
 var loader = document.getElementById("preloader");
-loader.style.display = "none";
 window.addEventListener("load", function () {
-  setTimeout(function () {
-    loader.style.opacity = "0";
-    loader.style.transition = "opacity 2s";
-    setTimeout(function () {
-      loader.style.display = "none";
-    }, 1000);
-  }, 1000); // Delay of 2 seconds (2000 milliseconds)
+  loader.style.display = "none";
 });
-
-// window.addEventListener("load", function () {
-//   loader.style.display = "none";
-// });
